27182d63e4d6ae57324ac6b3437c3ca143e923d7
SuperAnt <fauserfzy@gmail.com>
2021-10-08 14:38:48 +0800

    delete helloworld update

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,13 +1,14 @@
-import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
-import Home from '@/components/Home.vue'
+// import { expect } from 'chai'
+// import { shallowMount } from '@vue/test-utils'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('Home.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(Home, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).to.include(msg)
-  })
-})
+// describe('HelloWorld.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message'
+//     const wrapper = shallowMount(HelloWorld, {
+//       propsData: { msg }
+//     })
+//     expect(wrapper.text()).to.include(msg)
+//   })
+// })
+console.log('node-action test')
